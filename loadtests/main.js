42f7f39709a8fc3c338f9686964c10c11aca3f30
Robin Gustafsson <i@robingustafsson.com>
2017-06-02 16:25:38 +0200

    Lowered threshold

--- loadtests/main.js
+++ loadtests/main.js
@@ -9,7 +9,7 @@ export let options = {
         { duration: "60s", target: 0 }
     ],
     thresholds: {
-        http_req_duration: ["p(95)<500"]
+        http_req_duration: ["p(95)<100"]
     },
     ext: {
         loadimpact: {
